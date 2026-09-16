6bf21f827c9fca855d6448445c7c83091065b943
akmaladeem684-sys <akmaladeem684@gmail.com>
2026-09-16 23:27:21 +0500

    Fix TimelineCommand to use TimelineTrack model

--- app/src/main/java/com/example/domain/command/TimelineCommand.kt
+++ app/src/main/java/com/example/domain/command/TimelineCommand.kt
@@ -1,9 +1,9 @@
 package com.example.domain.command
 
-import com.example.domain.model.Track
+import com.example.domain.model.TimelineTrack
 
 interface TimelineCommand {
     val description: String
-    fun execute(tracks: List<Track>): List<Track>
-    fun undo(tracks: List<Track>): List<Track>
+    fun execute(tracks: List<TimelineTrack>): List<TimelineTrack>
+    fun undo(tracks: List<TimelineTrack>): List<TimelineTrack>
 }
