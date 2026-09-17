f401ca9c2704308e4e701ddc366bc8ce6cd008be
akmaladeem684-sys <akmaladeem684@gmail.com>
2026-09-17 23:48:15 +0500

    Preserve offscreen composition target state

--- app/src/main/cpp/render/NextGenGpuCompositionEngine.h
+++ app/src/main/cpp/render/NextGenGpuCompositionEngine.h
@@ -8,57 +8,19 @@
 #include "GpuRenderEngine.h"
 
 namespace ah_engine {
-
-struct GpuRenderStats {
-    uint64_t frames = 0;
-    uint64_t drawCalls = 0;
-    uint64_t shaderSwitches = 0;
-    uint64_t textureBinds = 0;
-    uint64_t fboSwitches = 0;
-    uint64_t skippedLayers = 0;
-    float lastFrameMs = 0.0f;
-};
-
+struct GpuRenderStats { uint64_t frames=0,drawCalls=0,shaderSwitches=0,textureBinds=0,fboSwitches=0,skippedLayers=0; float lastFrameMs=0.0f; };
 class NextGenGpuCompositionEngine {
 public:
-    NextGenGpuCompositionEngine();
-    ~NextGenGpuCompositionEngine();
-    bool init();
-    void resize(int width, int height);
-    void renderFrame(const std::vector<RenderLayer>& layers);
-    void renderExternalTexture(GLuint textureId, const float* texMatrix);
-    void beginOffscreen();
-    GLuint endOffscreen();
-    void onContextLost();
-    void release();
-    bool isInitialized() const { return initialized_; }
-    const GpuRenderStats& stats() const { return stats_; }
+    NextGenGpuCompositionEngine(); ~NextGenGpuCompositionEngine();
+    bool init(); void resize(int width,int height); void renderFrame(const std::vector<RenderLayer>& layers); void renderExternalTexture(GLuint textureId,const float* texMatrix);
+    void beginOffscreen(); GLuint endOffscreen(); void onContextLost(); void release();
+    bool isInitialized() const { return initialized_; } const GpuRenderStats& stats() const { return stats_; }
 private:
-    bool buildPrograms();
-    GLuint compile(GLenum type, const char* source);
-    void destroyPrograms();
-    void ensureTargets(int width, int height);
-    void destroyTargets();
-    void drawTexture(GLuint texture, const float* mvp, const float* st, float opacity, BlendMode mode, bool external);
-    void identity(float* m) const;
-    void multiply(float* out, const float* a, const float* b) const;
-    void layerMatrix(const RenderLayer& layer, float* out) const;
-    void blend(BlendMode mode);
-    void bindTarget(GLuint fbo, int width, int height);
-
-    bool initialized_ = false;
-    int width_ = 0, height_ = 0;
-    GLuint vao_ = 0, vbo_ = 0;
-    GLuint program2d_ = 0, programOes_ = 0;
-    GLint mvp2d_ = -1, st2d_ = -1, opacity2d_ = -1, sampler2d_ = -1;
-    GLint mvpOes_ = -1, stOes_ = -1, opacityOes_ = -1, samplerOes_ = -1;
-    GLuint fbo_[3] = {0, 0, 0};
-    GLuint target_[3] = {0, 0, 0};
-    int targetWidth_ = 0, targetHeight_ = 0, activeTarget_ = 0;
-    GpuRenderStats stats_;
-    float projection_[16]{};
-    GLuint lastProgram_ = 0, lastTexture_ = 0;
+    bool buildPrograms(); GLuint compile(GLenum type,const char* source); void destroyPrograms(); void ensureTargets(int width,int height); void destroyTargets();
+    void drawTexture(GLuint texture,const float* mvp,const float* st,float opacity,BlendMode mode,bool external); void identity(float* m) const; void multiply(float* out,const float* a,const float* b) const; void layerMatrix(const RenderLayer& layer,float* out) const; void blend(BlendMode mode); void bindTarget(GLuint fbo,int width,int height);
+    bool initialized_=false, offscreenActive_=false; int width_=0,height_=0; GLuint vao_=0,vbo_=0,program2d_=0,programOes_=0;
+    GLint mvp2d_=-1,st2d_=-1,opacity2d_=-1,sampler2d_=-1,mvpOes_=-1,stOes_=-1,opacityOes_=-1,samplerOes_=-1;
+    GLuint fbo_[3]={0,0,0},target_[3]={0,0,0}; int targetWidth_=0,targetHeight_=0,activeTarget_=0; GpuRenderStats stats_; float projection_[16]{}; GLuint lastProgram_=0,lastTexture_=0;
 };
-
 }
 #endif
